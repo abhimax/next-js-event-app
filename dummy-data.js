f3bbb5a1f1dc1170906341f44baa99588aac74ae
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-10-17 11:00:38 +0530

    feat: add dummy data and api

---
+++ dummy-data.js
@@ -0,0 +1,59 @@
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Programming for everyone",
+    description:
+      "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
+    location: "BMICH, Bauddhaloka Mawatha, Colombo 00700",
+    date: "2023-10-17",
+    image: "images/coding-event.jpg",
+    isFeatured: false,
+  },
+  {
+    id: "e2",
+    title: "Networking for introverts",
+    description:
+      "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
+    location:
+      "Nelum Pokuna Theatre, 110 Ananda Coomaraswamy Mawatha, Colombo 00700",
+    date: "2023-11-30",
+    image: "images/introvert-event.jpg",
+    isFeatured: true,
+  },
+  {
+    id: "e3",
+    title: "Networking for extroverts",
+    description:
+      "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
+    location:
+      "Sri Lanka Exhibition & Convention Centre, 12 McCallum Rd, Colombo 01000",
+    date: "2023-10-20",
+    image: "images/extrovert-event.jpg",
+    isFeatured: true,
+  },
+];
+
+export function getFeaturedEvents() {
+  return DUMMY_EVENTS.filter((event) => event.isFeatured);
+}
+
+export function getAllEvents() {
+  return DUMMY_EVENTS;
+}
+
+export function getFilteredEvents(dateFilter) {
+  const { year, month } = dateFilter;
+
+  let filteredEvents = DUMMY_EVENTS.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+}
+
+export function getEventById(id) {
+  return DUMMY_EVENTS.find((event) => event.id === id);
+}
