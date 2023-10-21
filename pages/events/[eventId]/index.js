d884622934fcf12f274ca040502166b84fcdbc77
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-10-21 12:56:47 +0530

    feat: get event route

--- pages/events/[eventId]/index.js
+++ pages/events/[eventId]/index.js
@@ -1,4 +1,10 @@
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+
 function EventDetailPage() {
-  return <div>Event Detail Page</div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  return <div>Event Detail Page {event.title}</div>;
 }
 export default EventDetailPage;
