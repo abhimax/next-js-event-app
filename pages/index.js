8081cf16a575ed8ebec93c20d710c580f64f994f
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-10-23 01:05:28 +0530

    feat: add layout component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import EventList from "@/components/events/EventList";
+import Layout from "@/components/layout/Layout";
 import { getFeaturedEvents } from "@/dummy-data";
 const events = getFeaturedEvents();
 export default function Home() {
-  return (
-    <>
-      <EventList events={events} />
-    </>
-  );
+  return <EventList events={events} />;
 }
