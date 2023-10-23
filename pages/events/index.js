f52355a7c25d6338a3292eaf1028d751665707bb
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-10-24 01:40:29 +0530

    feat: add all events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,4 +1,24 @@
+import EventItem from "@/components/events/EventItem";
+import { getAllEvents } from "@/dummy-data";
+
 function AllEvent() {
-  return <div>All Event</div>;
+  const events = getAllEvents();
+  return (
+    <>
+      {events.map((event) => {
+        return (
+          <EventItem
+            key={event.id}
+            id={event.id}
+            title={event.title}
+            description={event.description}
+            image={event.image}
+            location={event.location}
+            date={event.date}
+          />
+        );
+      })}
+    </>
+  );
 }
 export default AllEvent;
