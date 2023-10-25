643bae7307ae2b29c1beb5fd47161755a9bcf0af
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-10-25 06:32:32 +0530

    feat: add event search component

---
+++ components/events/EventSearch.js
@@ -0,0 +1,36 @@
+import Button from "../button/Button";
+
+function EventSearch(props) {
+  return (
+    <form>
+      <div>
+        <div>
+          <label htmlFor="year">Year</label>
+          <select id="year">
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="month">Year</label>
+          <select id="month">
+            <option value="January">January</option>
+            <option value="February">February</option>
+            <option value="March">March</option>
+            <option value="April">April</option>
+            <option value="May">May</option>
+            <option value="June">June</option>
+            <option value="July">July</option>
+            <option value="August">August</option>
+            <option value="September">September</option>
+            <option value="October">October</option>
+            <option value="November">November</option>
+            <option value="December">December</option>
+          </select>
+        </div>
+      </div>
+      <Button>Find Events</Button>
+    </form>
+  );
+}
+export default EventSearch;
