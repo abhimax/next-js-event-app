724329c8c46388a15b4e3893e01a747f3bf9f5e7
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-10-29 01:18:44 +0530

    feat: apply filter router

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,11 +2,13 @@ import EventItem from "@/components/events/EventItem";
 import EventList from "@/components/events/EventList";
 import EventSearch from "@/components/events/EventSearch";
 import { getAllEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 
 function AllEvent() {
+  const router = useRouter();
   const events = getAllEvents();
   const onEventFilterHandler = (data) => {
-    console.log(data);
+    router.push(`/events/${data.selectedYear}/${data.selectedMonth}`);
   };
   return (
     <>
