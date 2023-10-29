0f54fae0647d43356eeb8ea7b89edf4528525607
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-10-29 14:19:53 +0530

    feat: get filtered events

--- pages/events/[...slug]/index.js
+++ pages/events/[...slug]/index.js
@@ -1,4 +1,22 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+
 function FilteredEvent() {
+  const router = useRouter();
+  const filteredData = router.query.slug;
+  if (!filteredData) {
+    return <p className="center">Loading...</p>;
+  }
+  const filteredYear = +filteredData[0];
+  const filteredMonth = +filteredData[1];
+
+  console.log(filteredData, filteredYear, filteredMonth);
+
+  const filteredEvent = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+  console.log(filteredEvent);
   return <div>Filtered Event</div>;
 }
 export default FilteredEvent;
